e564839a8a842a98b3efe25bf562898213ba194f
daniellin9406 <universetennis@gmail.com>
2017-07-21 00:54:08 +0800

    set envir

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,50 @@ module.exports = {
   context: path.resolve(__dirname, './src'),
   entry: {
     app: './main.js',
+    // Multiple files, bundled together
+    // app:['./home.js','./events.js','./vendor.js'],
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './dist/assets'),
     filename: '[name].bundle.js',
+    publicPath: '/assets',
+  },
+  devServer:{
+    contentBase: path.resolve(__dirname, './src'),
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'commons',
+      filename: 'commons.js',
+      minChunks: 2,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2017'] },
+        }],
+      },
+      // Loaders for other file types can go here
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, './src'), 'node_modules']
   },
 };
